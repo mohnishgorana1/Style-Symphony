495ff8c10457b0d64193822bc57e885580c27a8d
mohnishgorana1 <mohnishgorana1@gmail.com>
2024-01-02 22:34:23 +0530

    product-model made

---
+++ server/src/models/products.model.js
@@ -0,0 +1,43 @@
+import mongoose, { Schema, model } from "mongoose";
+import bcrypt from "bcryptjs";
+import Jwt from "jsonwebtoken";
+import crypto from "crypto";
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      minLength: [5, "Name must be at least 5 char"],
+      maxLength: [20, "Name must be at less than 20 char"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: [true, "Price is required"],
+    },
+    category: {
+      type: String,
+      enum: ["Male", "Female"],
+      required: true,
+    },
+    images: [
+      {
+        secure_url: {
+          type: String,
+        },
+        public_id: {
+          type: String,
+        },
+        z,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Product = mongoose.model("Product", productSchema);
